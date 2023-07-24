decc893bc098035ebfb508fe7306765263f1c9be
Manny Delgado <mannygdp@gmail.com>
2023-07-23 23:19:45 -0700

    working

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+    res.send('Hello world!');
+});
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log('app is listening on port 3000');
+});
